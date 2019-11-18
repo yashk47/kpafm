3b742dea6179851ff8f7f2800ccac3a02d45470e
yashk47 <iyash@outlook.com>
2019-11-18 17:40:58 +0530

    mid work 1

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -23,22 +23,19 @@ class MobileMenu extends Component{
                             <li className="menu-item-has-children">
                             <a href={`${process.env.PUBLIC_URL}/`}>Home</a>
                             </li>
-                            <li><a href={`${process.env.PUBLIC_URL}/about-us`}>ABOUT</a></li>
+                            {/* <li><a href={`${process.env.PUBLIC_URL}/about-us`}>ABOUT</a></li> */}
                             <li className="menu-item-has-children">
                                 <a href={`${process.env.PUBLIC_URL}/services`}>SERVICE</a>
                                 <ul className="sub-menu">
-                                    <li><a href={`${process.env.PUBLIC_URL}/services`}>Service Page</a></li>
-                                    <li><a href={`${process.env.PUBLIC_URL}/service-details-left-sidebar`}>Service Details Left Sidebar</a></li>
-                                    <li><a href={`${process.env.PUBLIC_URL}/service-details-right-sidebar`}>Service Details Right Sidebar</a></li>
+                                <li><a href={`${process.env.PUBLIC_URL}/services`}>Fetomaternal Medicine</a></li>
+                                <li><a href={`${process.env.PUBLIC_URL}/services`}>Infertility</a></li>
+                                <li><a href={`${process.env.PUBLIC_URL}/services`}>Endoscopic Surgery</a></li>
+                                <li><a href={`${process.env.PUBLIC_URL}/services`}>Obstetrics and Gynaecology</a></li>
+                                <li><a href={`${process.env.PUBLIC_URL}/services`}>Neonatology</a></li>
+                                <li><a href={`${process.env.PUBLIC_URL}/services`}>Genetics</a></li>
                                 </ul>
                             </li>
-                            <li className="menu-item-has-children">
-                                <a href={`${process.env.PUBLIC_URL}/projects`} >PROJECT</a>
-                                <ul className="sub-menu">
-                                    <li><a href={`${process.env.PUBLIC_URL}/projects`} >Project Page</a></li>
-                                    <li><a href={`${process.env.PUBLIC_URL}/project-details`}>Project Details</a></li>
-                                </ul>
-                            </li>
+{/*                             
                             <li className="menu-item-has-children">
                                 <a href={`${process.env.PUBLIC_URL}/blog-left-sidebar`}>BLOG</a>
                                 <ul className="sub-menu">
@@ -46,8 +43,8 @@ class MobileMenu extends Component{
                                     <li><a href={`${process.env.PUBLIC_URL}/blog-right-sidebar`}>Blog Right Sidebar</a></li>
                                     <li><a href={`${process.env.PUBLIC_URL}/blog-details-left-sidebar`}>Blog Details Left Sidebar</a></li>
                                     <li><a href={`${process.env.PUBLIC_URL}/blog-details-right-sidebar`}>Blog Details Right Sidebar</a></li>
-                                </ul>
-                            </li>
+                                </ul> 
+                            </li> */}
                             <li><a href={`${process.env.PUBLIC_URL}/contact-us`}>CONTACT</a> </li>
                             </ul>
                         </nav>
@@ -55,7 +52,7 @@ class MobileMenu extends Component{
                             <div className="off-canvas-contact-widget">
                             <div className="header-contact-info">
                                 <ul className="header-contact-info__list">
-                                <li><i className="ion-android-phone-portrait" /> <a href="tel://12452456012">(1245) 2456 012 </a></li>
+                                <li><i className="ion-android-phone-portrait" /> <a href="tel://12452456012">(+91) 98297 79626</a></li>
                                 <li><i className="ion-android-mail" /> <a href="mailto:info@yourdomain.com">info@yourdomain.com</a></li>
                                 </ul>
                             </div>
@@ -66,7 +63,7 @@ class MobileMenu extends Component{
                             <a href="#/" title="Twitter"><i className="fa fa-twitter" /></a>
                             <a href="#/" title="LinkedIn"><i className="fa fa-linkedin" /></a>
                             <a href="#/" title="Youtube"><i className="fa fa-youtube-play" /></a>
-                            <a href="#/" title="Vimeo"><i className="fa fa-vimeo-square" /></a>
+                            {/* <a href="#/" title="Vimeo"><i className="fa fa-vimeo-square" /></a> */}
                             </div>
                             {/*Off Canvas Widget Social End*/}
                         </div>
